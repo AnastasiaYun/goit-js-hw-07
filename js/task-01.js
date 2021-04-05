1daeb2333e18b68fc98b604b465ad52c662d361e
AnastasiaYun <tigrawom@gmail.com>
2021-04-05 23:10:23 +0300

    update

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+
+const allItems = document.querySelectorAll('.item');
+console.log(`В списке ${allItems.length} категории.`);
+
+
+const listItem = document.querySelector('.item')
+console.log('Категория:', allItems[0].firstElementChild.textContent);
+console.log(`Количество элементов: ${allItems[0].lastElementChild.children.length}`);
+
+
+console.log('Категория:', allItems[1].firstElementChild.textContent);
+console.log(`Количество элементов: ${allItems[1].lastElementChild.children.length}`);
+
+console.log('Категория:',  allItems[2].firstElementChild.textContent);
+console.log(`Количество элементов: ${allItems[2].lastElementChild.children.length}`);
