a94fbd1c787abd33547a1cd791089b2e2b5f9c20
AnastasiaYun <tigrawom@gmail.com>
2021-04-12 00:06:01 +0300

    1

--- js/task-03.js
+++ js/task-03.js
@@ -17,9 +17,25 @@ const images = [
   ];
 
   const galleryEl = document.getElementById('gallery');
-  galleryEl.insertAdjacentHTML('afterbegin','<img src="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" width="800">');
-  galleryEl.insertAdjacentHTML('beforeend','<img src="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" width="700">');
-  galleryEl.insertAdjacentHTML('beforeend','<img src="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" width="700">');
+  galleryEl.style.width = 700;
+
+  const elements = images.map(image => {
+
+    const elLi = document.createElement('li');
+    elLi.insertAdjacentHTML("afterbegin",`<img src='${image.url}' alt='${image.alt}' width='750px'>`);
+
+    return elLi;
+  });
+
+  
+  galleryEl.append(...elements);
+
+
+  // galleryEl.insertAdjacentHTML('afterbegin','<img src="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" width="800">');
+  // galleryEl.insertAdjacentHTML('beforeend','<img src="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" width="700">');
+  // galleryEl.insertAdjacentHTML('beforeend','<img src="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" width="700">');
+  // galleryEl.style.width = 700;
+  
 
 
 
