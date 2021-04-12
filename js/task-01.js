5e8941ce4e564b44641588e847d9115d02398ede
AnastasiaYun <tigrawom@gmail.com>
2021-04-12 22:22:31 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,13 @@
 
-const allItems = document.querySelectorAll('.item');
-console.log(`В списке ${allItems.length} категории.`);
 
 
-const listItem = document.querySelector('.item')
-console.log('Категория:', allItems[0].firstElementChild.textContent);
-console.log(`Количество элементов: ${allItems[0].lastElementChild.children.length}`);
+const allitems = document.querySelectorAll('li.item');
+const items = allitems.length;
+console.log(`В списке ${items} категории.`);
 
+const categories = allitems.forEach(item => {
+    console.log(`Категория: ${item.querySelector('h2').textContent}`);
 
-console.log('Категория:', allItems[1].firstElementChild.textContent);
-console.log(`Количество элементов: ${allItems[1].lastElementChild.children.length}`);
-
-console.log('Категория:',  allItems[2].firstElementChild.textContent);
-console.log(`Количество элементов: ${allItems[2].lastElementChild.children.length}`);
+    console.log(`Количество элементов: ${item.querySelectorAll('li').length}`);
+}
+);
